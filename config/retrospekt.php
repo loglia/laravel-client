a6c317a395f5ee83aaa8f29c50d8cbd5b752c8e0
Chris White <chris@cwhite.me>
2019-05-25 23:57:33 -0400

    added api_key to config file

--- config/retrospekt.php
+++ config/retrospekt.php
@@ -2,4 +2,9 @@
 
 return [
 
+    /**
+     * Your API key is an application-specific secret key. You can find it in your application settings.
+     */
+    'api_key' => env('RETROSPEKT_API_KEY'),
+
 ];
