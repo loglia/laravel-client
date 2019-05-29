25d724e0ffa2a8228b5a770155204f4caa766429
Chris White <chris@cwhite.me>
2019-05-28 20:51:52 -0400

    test skeleton

---
+++ tests/Unit/Monolog/RetrospektFormatterTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Retrospekt\LaravelClient\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class RetrospektFormatterTest extends TestCase
+{
+    /** @test */
+    public function it_normalizes_exceptions_into_retrospekt_format()
+    {
+        
+    }
+
+    /** @test */
+    public function it_moves_exceptions_in_context_to_extra()
+    {
+        
+    }
+
+    /** @test */
+    public function it_moves_throwables_in_context_to_extra()
+    {
+        
+    }
+
+    /** @test */
+    public function it_removes_exception_from_context_when_moving_to_extra()
+    {
+
+    }
+
+    /** @test */
+    public function it_serializes_result_to_json()
+    {
+        
+    }
+}
