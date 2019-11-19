9d8bb0848d22cc3df046cbfd1a20ff94e4be9f7c
Chris White <cwhite@intouchinsight.com>
2019-11-19 17:26:36 -0500

    Register Loglia as a logging channel

--- src/LaravelClientServiceProvider.php
+++ src/LaravelClientServiceProvider.php
@@ -4,6 +4,10 @@ namespace Loglia\LaravelClient;
 
 use Illuminate\Support\ServiceProvider;
 use Loglia\LaravelClient\Middleware\LogHttp;
+use Loglia\LaravelClient\Monolog\LogliaFormatter;
+use Loglia\LaravelClient\Monolog\LogliaHandler;
+use Loglia\LaravelClient\Sticky\StickyContextProcessor;
+use Monolog\Logger;
 
 class LaravelClientServiceProvider extends ServiceProvider
 {
@@ -17,6 +21,27 @@ class LaravelClientServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/loglia.php', 'loglia');
+
+        $this->app['log']->extend('loglia', function ($app, array $config) {
+            $logger = new Logger('loglia');
+
+            $handler = new LogliaHandler;
+
+            if (config('loglia.api_key')) {
+                $handler->setApiKey(config('loglia.api_key'));
+            }
+
+            if (config('loglia.endpoint')) {
+                $handler->setEndpoint(config('loglia.endpoint'));
+            }
+
+            $handler->setFormatter(new LogliaFormatter(\DateTime::ISO8601));
+
+            $handler->pushProcessor(new StickyContextProcessor);
+            $logger->pushHandler($handler);
+
+            return $logger;
+        });
 
         $this->app->singleton(LogHttp::class);
     }
