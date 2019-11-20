1ee725fdecafba08ceda2adb89bfe47fa3a3d268
Chris White <cwhite@intouchinsight.com>
2019-11-19 23:47:44 -0500

    Make setUpLogger a static function

--- src/LaravelClientServiceProvider.php
+++ src/LaravelClientServiceProvider.php
@@ -23,18 +23,18 @@ class LaravelClientServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/../config/loglia.php', 'loglia');
 
+        // TODO: singleton may not be needed
+        $this->app->singleton(LogHttp::class);
+
         if ($this->app['log'] instanceof LogManager) {
             // In L5.6+, extend the log component with a loglia driver.
             $this->app['log']->extend('loglia', function ($app, array $config) {
-                return $this->setUpLogger(new Logger('loglia'));
+                return LaravelClientServiceProvider::setUpLogger(new Logger('loglia'));
             });
         } else {
             // In older Laravel versions, modify Monolog to use the Loglia handler.
-            $this->setUpLogger($this->app['log']->getMonolog());
+            LaravelClientServiceProvider::setUpLogger($this->app['log']->getMonolog());
         }
-
-
-        $this->app->singleton(LogHttp::class);
     }
 
     /**
@@ -43,7 +43,7 @@ class LaravelClientServiceProvider extends ServiceProvider
      * @param Logger $logger
      * @return Logger
      */
-    private function setUpLogger(Logger $logger)
+    public static function setUpLogger(Logger $logger)
     {
         $handler = new LogliaHandler;
 
