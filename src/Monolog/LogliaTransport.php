a12c0450187e03735cbfdb41dfd52273279fd05f
Chris White <cwhite@intouchinsight.com>
2020-05-24 00:00:55 -0400

    CURLOPT_RETURNTRANSFER

--- src/Monolog/LogliaTransport.php
+++ src/Monolog/LogliaTransport.php
@@ -26,9 +26,10 @@ class LogliaTransport implements TransportInterface
         $postData = json_encode($logs, JSON_THROW_ON_ERROR);
 
         $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, $this->endpoint);
         curl_setopt($ch, CURLOPT_TIMEOUT, 3);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
             sprintf('Authorization: %s', $this->apiKey),
